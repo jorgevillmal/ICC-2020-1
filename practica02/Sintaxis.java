a9ca2b2f9196aa42aee32507410272b5ae0b9e32
jorgevillmal <53312702+jorgevillmal@users.noreply.github.com>
2019-08-24 21:57:33 -0500

    Add files via upload

---
+++ practica02/Sintaxis.java
@@ -0,0 +1,29 @@
+import static java.lang.Math.sqrt;
+
+public class Sintaxis {
+
+        public static void main(String[] args) {
+                int a, b = 20, c;
+                a = 10; c = -7;
+
+                boolean prop = true;
+                byte ochoBits = 127;
+                short dieciseisBits = -32767;
+
+                double discriminante = b * b - 4 * a * c;
+                double x1 = (-b + sqrt(discriminante)) / (2 * a); 
+                double x2 = (-b - sqrt(discriminante)) / (2 * a);
+
+                char ch1 = 'g'; 
+		char ch2 = 'w';
+                int  d = 1 / (a - 3);
+
+                // Declaraciones
+
+		long var_1 = 214783648L;
+		char quote = '\'';
+
+        }
+
+}
+
