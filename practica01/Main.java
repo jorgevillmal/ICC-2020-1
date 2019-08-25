509919cb32915b9567c474132d07668e6eeefe2a
jorgevillmal <53312702+jorgevillmal@users.noreply.github.com>
2019-08-24 21:55:30 -0500

    Add files via upload

---
+++ practica01/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+
+    public static void main(String[] args){
+
+	System.out.println("\n\n$ java --version");
+	System.out.println("java 12.0.2 2019-07-16\nJava(TM) SE Runtime Environment (build 12.0.2+10)\nJava HotSpot(TM) 64-Bit Server VM (build 12.0.2+10, mixed mode, sharing)");
+	
+	System.out.println("\nDarwin MacBook-Pro-de-jose.local 18.7.0 Darwin Kernel Version 18.7.0: Thu Jun 20 18:42:21\n");
+
+  }
+}
