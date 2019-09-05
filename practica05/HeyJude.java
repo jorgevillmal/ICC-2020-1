f515e131a8d398923f1d534c4e95a745b0601414
jorgevillmal <53312702+jorgevillmal@users.noreply.github.com>
2019-09-04 23:56:58 -0500

    Add files via upload

---
+++ practica05/HeyJude.java
@@ -0,0 +1,163 @@
+public class HeyJude{
+	public static void  main(String args[]) {
+		int i = 0, j = 0, k = 0;
+		String a = "hey Jude";
+		String b = "don't";
+		String c = "make it bad";
+		String d = "be afraid";
+		String e = "let me down";
+		String f = "take a sad song and make it better";
+		String g = "you were made to go out and get free";
+		String h = "you have found her, now go and get her";
+		String l = "remember to";
+		String m = "let her into you heart";
+		String n = "let her under your skin";
+		String o = "then you";
+		String p = "can start";
+		String q = "begin";
+		String r = "to make it better";
+		String s = "better better better better better ho";
+		String t = "na";
+
+
+		while(i != 7)
+		{
+
+			if( i == 0) {
+				System.out.println(" ");
+				System.out.print(a + " ");
+
+				System.out.print(b + " ");
+				System.out.println(c);
+				System.out.println(f);
+				System.out.print(l + " ");
+				System.out.println(m);
+				System.out.print(o + " ");
+				System.out.print(p + " ");
+				System.out.println(r);
+					
+			
+				System.out.println(" "); 			
+			}
+	
+			if ( i == 1) 
+			{
+		
+				System.out.println(" ");
+                                System.out.print(a + " ");
+
+                                System.out.print(b + " ");
+                                System.out.println(d);
+                                System.out.println(g);
+                                System.out.print(l + " ");
+                                System.out.println(n);
+                                System.out.print(o + " ");
+                                System.out.print(q + " ");
+                                System.out.println(r);
+
+
+                                System.out.println(" ");
+		
+			}
+	
+			if (i == 2) 
+			{
+		
+				System.out.println(" ");
+
+				while(k != 9) {
+
+					System.out.print(t + " ");
+					k++;					
+
+				}
+
+                                System.out.println(" ");
+
+			}
+	
+			k = 0;
+
+			if ( i == 3 ) 
+			{
+
+				System.out.println(" ");
+                                System.out.print(a + " ");
+
+                                System.out.print(b + " ");
+                                System.out.println(e);
+                                System.out.println(h);
+                                System.out.print(l + " ");
+                                System.out.println(m);
+                                System.out.print(o + " ");
+                                System.out.print(p + " ");
+                                System.out.println(r);
+
+
+                                System.out.println(" ");
+	
+			}
+
+			if ( i == 4 )
+                        {
+                                
+                                System.out.println(" ");
+                                
+                                while(k != 9) {
+                                        
+                                        System.out.print(t + " ");
+                                        k++;
+                                
+                                }
+                                
+                                System.out.println(" ");
+                        
+                        }
+	
+			k = 0;
+
+			if ( i == 5 )
+                        {
+
+                                System.out.println(" ");
+                                System.out.print(a + " ");
+
+                                System.out.print(b + " ");
+                                System.out.println(c);
+                                System.out.println(f);
+                                System.out.print(l + " ");
+                                System.out.println(n);
+                                System.out.print(o + " ");
+                                System.out.print(q + " ");
+                                System.out.println(r);
+				System.out.println(s);
+
+
+                                System.out.println(" ");
+
+                        }
+
+			k = 0;
+
+			if ( i == 6 )
+                        {
+                       
+                                System.out.println(" ");
+
+                                while(k != 9) {
+
+                                        System.out.print(t + " ");
+                                        k++;
+
+                                }
+				
+				System.out.print(a);
+                                System.out.println(" ");
+
+                        }
+	
+			System.out.println(" ");	
+			i++;
+		}	
+	}
+}
